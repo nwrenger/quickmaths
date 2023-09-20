d0ef75f4b5316a87535dd512c39e3e4e178e4b29
NWrenger <nils@wrenger.net>
2023-09-20 08:59:05 +0200

    :wrench: Fixed fmt

--- src/routes/learn/LearnView.svelte
+++ src/routes/learn/LearnView.svelte
@@ -67,109 +67,107 @@
 	}
 </script>
 
-<section>
-	<div class="row align-items-center mx-auto">
+<div class="row align-items-center mx-auto">
+	<div class="col p-0">
 		<!-- svelte-ignore a11y-missing-content -->
-		<div class="col p-0">
-			<a class="btn btn-close col text-start" href="/learn" />
-		</div>
-		<div class="text-center p-0 d-flex col justify-content-end">
-			Lives:
-			<span class="badge text-bg-danger text-wrap ms-2">
-				{lives < 0 ? 'No More lives! The answers will be Shown!' : lives}
-			</span>
-		</div>
+		<a class="btn btn-close col text-start" href="/learn" />
 	</div>
+	<div class="text-center p-0 d-flex col justify-content-end">
+		Lives:
+		<span class="badge text-bg-danger text-wrap ms-2">
+			{lives < 0 ? 'No More lives! The answers will be Shown!' : lives}
+		</span>
+	</div>
+</div>
 
-	<ul class="nav nav-pills mb-3 mt-2" id="tab" role="tablist">
-		{#each quests as _quest, index}
-			<li class="nav-item" role="presentation">
-				<button
-					class="nav-link {index === 0 ? 'active' : ''}"
-					id="Question-{index}-tab"
-					data-bs-toggle="pill"
-					data-bs-target="#Question-{index}"
-					type="button"
-					role="tab"
-					aria-controls="Question-{index}">Question {index + 1}</button
-				>
-			</li>
-		{/each}
-		<li class="nav-item" role="presentation" />
-	</ul>
-	<div class="tab-content" id="pills-tabContent">
-		{#each quests as quest, index}
-			<div
-				class="tab-pane fade {index === 0 ? 'show active' : ''}"
-				id="Question-{index}"
-				role="tabpanel"
-				aria-labelledby="Question-{index}-tab"
-				tabindex="0"
+<ul class="nav nav-pills mb-3 mt-2" id="tab" role="tablist">
+	{#each quests as _quest, index}
+		<li class="nav-item" role="presentation">
+			<button
+				class="nav-link {index === 0 ? 'active' : ''}"
+				id="Question-{index}-tab"
+				data-bs-toggle="pill"
+				data-bs-target="#Question-{index}"
+				type="button"
+				role="tab"
+				aria-controls="Question-{index}">Question {index + 1}</button
 			>
-				<div class="mb-3">
-					<label for="Question-{index}-input" class="form-label">What is {quest.question}?</label>
-					<input
-						type="number"
-						class="form-control {quest.isIncorrect
-							? 'is-invalid'
-							: quest.isIncorrect === false
-							? 'is-valid'
-							: ''}"
-						id="Question-{index}-input"
-						placeholder="Input answer..."
-						required
-					/>
-					<div class="valid-feedback">Looks good!</div>
-					<div class="invalid-feedback">
-						Wrong, The correct value would be {lives >= 0
-							? quest.userInput
-								? quest.answer > quest.userInput
-									? 'Bigger'
-									: 'Smaller'
-								: 'Idk, Wrong Input'
-							: quest.answer}!
-					</div>
+		</li>
+	{/each}
+	<li class="nav-item" role="presentation" />
+</ul>
+<div class="tab-content" id="pills-tabContent">
+	{#each quests as quest, index}
+		<div
+			class="tab-pane fade {index === 0 ? 'show active' : ''}"
+			id="Question-{index}"
+			role="tabpanel"
+			aria-labelledby="Question-{index}-tab"
+			tabindex="0"
+		>
+			<div class="mb-3">
+				<label for="Question-{index}-input" class="form-label">What is {quest.question}?</label>
+				<input
+					type="number"
+					class="form-control {quest.isIncorrect
+						? 'is-invalid'
+						: quest.isIncorrect === false
+						? 'is-valid'
+						: ''}"
+					id="Question-{index}-input"
+					placeholder="Input answer..."
+					required
+				/>
+				<div class="valid-feedback">Looks good!</div>
+				<div class="invalid-feedback">
+					Wrong, The correct value would be {lives >= 0
+						? quest.userInput
+							? quest.answer > quest.userInput
+								? 'Bigger'
+								: 'Smaller'
+							: 'Idk, Wrong Input'
+						: quest.answer}!
 				</div>
-				{#if quests[index + 1]}
-					<button
-						type="button"
-						class="btn btn-primary"
-						on:click={() => {
-							if (advance(`Question-${index + 1}`) != 'wtf') index += 1;
-						}}>Next</button
-					>
-				{:else}
-					<button
-						type="button"
-						class="btn btn-danger"
-						on:click={() => {
-							getAnswers(index);
-							if (validateAnswers()) {
-								correctDialog.open();
-							} else {
-								lives -= 1;
-							}
-							if (lives < -1) incorrectDialog.open();
-						}}>Finish</button
-					>
-				{/if}
 			</div>
-		{/each}
+			{#if quests[index + 1]}
+				<button
+					type="button"
+					class="btn btn-primary"
+					on:click={() => {
+						if (advance(`Question-${index + 1}`) != 'wtf') index += 1;
+					}}>Next</button
+				>
+			{:else}
+				<button
+					type="button"
+					class="btn btn-danger"
+					on:click={() => {
+						getAnswers(index);
+						if (validateAnswers()) {
+							correctDialog.open();
+						} else {
+							lives -= 1;
+						}
+						if (lives < -1) incorrectDialog.open();
+					}}>Finish</button
+				>
+			{/if}
+		</div>
+	{/each}
+</div>
+<Dialog bind:this={correctDialog}>
+	<span slot="header"> Info </span>
+	<div class="text-center" slot="body">
+		<h3 class="text-success">Congratulations!</h3>
+		<p>You did it correct!</p>
+		<a class="btn btn-primary" href="/learn">Ok!</a>
 	</div>
-	<Dialog bind:this={correctDialog}>
-		<span slot="header"> Info </span>
-		<div class="text-center" slot="body">
-			<h3 class="text-success">Congratulations!</h3>
-			<p>You did it correct!</p>
-			<a class="btn btn-primary" href="/learn">Ok!</a>
-		</div>
-	</Dialog>
-	<Dialog bind:this={incorrectDialog}>
-		<span slot="header"> Info </span>
-		<div class="text-center" slot="body">
-			<h3 class="text-danger">No more Lives!</h3>
-			<p>You couldn't do it..I know..</p>
-			<a class="btn btn-primary" href="/learn">Ok!</a>
-		</div>
-	</Dialog>
-</section>
+</Dialog>
+<Dialog bind:this={incorrectDialog}>
+	<span slot="header"> Info </span>
+	<div class="text-center" slot="body">
+		<h3 class="text-danger">No more Lives!</h3>
+		<p>You couldn't do it..I know..</p>
+		<a class="btn btn-primary" href="/learn">Ok!</a>
+	</div>
+</Dialog>
