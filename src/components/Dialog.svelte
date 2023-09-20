780dbbcccf33a8128c75396a4c1ba28b261ab540
nwrenger <nils@wrenger.net>
2023-09-20 22:24:27 +0200

    :sparkles: Improved Modal

--- src/components/Dialog.svelte
+++ src/components/Dialog.svelte
@@ -10,8 +10,18 @@
 
 <dialog class="custom-dialog" bind:this={dialog} on:close>
 	<div class="card">
-		<div class="card-header">
+		<div class="card-header d-flex justify-content-between">
 			<slot name="header" />
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<a
+				type="button"
+				class="btn-close justify-content-end"
+				data-bs-dismiss="modal"
+				aria-label="Close"
+				on:click={() => dialog.close()}
+			/>
 		</div>
 		<div class="card-body">
 			<slot name="body" />
