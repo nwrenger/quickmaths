0b18eb4964586cb06e4fb362a0e0bd3c397522ef
NWrenger <nils@wrenger.net>
2023-10-06 17:12:06 +0200

    :sparkles: Adjusted Difficulty

--- src/routes/learn/pow/+page.svelte
+++ src/routes/learn/pow/+page.svelte
@@ -9,10 +9,10 @@
 	function gen(count: number): Quests[] {
 		let quests: Quests[] = [];
 		for (let i = 1; i < count + 1; i++) {
-			let a = getRandomInt(2 * difficulty, 12 * difficulty);
+			let a = getRandomInt(2 * difficulty, 6 * difficulty);
 			let b = getRandomInt(2, difficulty > 1 ? (difficulty > 3 ? 4 : 3) : 2);
 			while (quests.find((quest) => quest.answer == a ** b)) {
-				a = getRandomInt(2 * difficulty, 12 * difficulty);
+				a = getRandomInt(2 * difficulty, 6 * difficulty);
 				b = getRandomInt(2, difficulty > 1 ? (difficulty > 3 ? 4 : 3) : 2);
 			}
 			quests.push({ question: `${a} ^ ${b}`, answer: a ** b });
