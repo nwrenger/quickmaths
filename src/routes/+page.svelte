6ead3b4699cad60189fcbca92f7e3e793abf2aa2
nwrenger <nils@wrenger.net>
2024-03-22 02:50:57 +0100

    :wrench: Renaming

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <svelte:head>
-	<title>Quickmaths v2</title>
+	<title>Quickmaths</title>
 	<meta name="description" content="Start your journey with QuickMaths by selecting a mode." />
 </svelte:head>
 
 <!--  Welcome Container with game selection  -->
 <div class="container h-full mx-auto flex justify-center">
 	<div class="space-y-5 flex flex-col items-center text-center lg:w-2/3 w-11/12 pt-4 pb-4">
-		<h2 class="h2">Welcome to QuickMaths v2!</h2>
+		<h2 class="h2">Welcome to QuickMaths!</h2>
 		<p>
 			Embark on a fun journey to master calculations. Whether you want to learn at your own pace or
 			challenge a friend, we've got you covered.
